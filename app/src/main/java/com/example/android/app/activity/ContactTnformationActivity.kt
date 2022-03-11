815d3a509534b3089df436349955961efe8e1b4f
Rohit1372 <rohitv1372@gmail.com>
2022-03-11 13:37:31 +0530

    Add more activities

---
+++ app/src/main/java/com/example/android/app/activity/ContactTnformationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.android.app.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactTnformationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_tnformation)
+    }
+}
